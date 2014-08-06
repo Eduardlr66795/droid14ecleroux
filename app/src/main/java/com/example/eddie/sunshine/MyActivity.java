6828e718a027c71d20a34f09ff2c9aa8d0e809be
Heinrich Dahms <htdahms@gmail.com>
2014-08-06 20:29:38 +0200

    End of L2

--- app/src/main/java/com/example/eddie/sunshine/MyActivity.java
+++ app/src/main/java/com/example/eddie/sunshine/MyActivity.java
@@ -1,20 +1,9 @@
 package com.example.eddie.sunshine;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
 
 
 public class MyActivity extends ActionBarActivity {
@@ -27,7 +16,7 @@ public class MyActivity extends ActionBarActivity {
         setContentView(R.layout.activity_my);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -37,6 +26,11 @@ public class MyActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.my, menu);
+        getMenuInflater().inflate(R.menu.forecastfragment, menu);
+//        getMenuInflater().inflate(R.menu.menu_test, menu);
+
+
+
         return true;
     }
 
@@ -45,67 +39,14 @@ public class MyActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
+
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
         }
+
+//        if(id = item.getItemId())
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        private ArrayAdapter<String> forecast;
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_my, container, false);
-
-            String forecastArray[] = {
-                    "Today - Sunny - 88/63",
-                    "Tomorrow - Sunny - 88/63",
-                    "Wednesday - Sunny - 88/63",
-                    "Thursday - Sunny - 88/63",
-                    "Friday - Sunny - 88/63"
-            };
-
-            ArrayList<String> forecastList = new ArrayList<String>(Arrays.asList(forecastArray));
-
-            forecast = new ArrayAdapter<String>(
-                getActivity(),
-                R.layout.list_item_forecast,
-                R.id.list_item_forecast_textview,
-                forecastList);
-
-
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(forecast);
-
-
-//            Calendar cal = Calendar.getInstance();
-//            Intent intent = new Intent(Intent.ACTION_EDIT);
-//            intent.setType("vnd.android.cursor.item/event");
-//            intent.putExtra("beginTime", cal.getTimeInMillis());
-//            intent.putExtra("allDay", true);
-//            intent.putExtra("rrule", "FREQ=YEARLY");
-//            intent.putExtra("endTime", cal.getTimeInMillis()+60*60*1000);
-//            intent.putExtra("title", "A Test Event from android app");
-//            startActivity(intent);
-
-
-            return rootView;
-        }
-
-
-
-
-
-
-    }
 }
---
+++ app/src/main/java/com/example/eddie/sunshine/WeatherDataParser.java
@@ -0,0 +1,21 @@
+package com.example.eddie.sunshine;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Created by eddie on 2014/08/06.
+ */
+public class WeatherDataParser {
+
+
+    public static Double getMaxTemperatureForDay(String weatherJsonStr, int dayIndex)
+    throws JSONException {
+        JSONObject weather = new JSONObject(weatherJsonStr);
+        JSONArray days = weather.getJSONArray("list");
+        JSONObject dayInfo = days.getJSONObject(dayIndex);
+        JSONObject temperatureInfo = dayInfo.getJSONObject("temp");
+        return temperatureInfo.getDouble("max");
+    }
+}
